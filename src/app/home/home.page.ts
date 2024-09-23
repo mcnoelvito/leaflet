8ea3dcbd05d0ca28a8aa5b9ce598f85b8c4061cc
mcnoelvito <noelvitohu@gmail.com>
2024-09-23 09:52:27 +0700

    import leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map!: L.Map;
 
   constructor() {}
-
+  ngOnInit() {
+  this.map = L.map('mapId').setView([35.76943, -580081])
+  }
 }
