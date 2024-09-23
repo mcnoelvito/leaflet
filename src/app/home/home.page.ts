71bb721c0b4d4ae634c2a1ab389a3239356e35f2
mcnoelvito <noelvitohu@gmail.com>
2024-09-23 10:05:30 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,8 +14,7 @@ export class HomePage {
   }
 
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView([35.76943, -580081])
-
+    this.map = L.map('mapId').setView([35.76943, -580081], 10)
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy, <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(this.map);
