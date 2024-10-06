dc4f16dda347887597caa247ba9b1a2536666379
mcnoelvito <noelvitohu@gmail.com>
2024-10-06 22:31:34 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -38,17 +38,19 @@ export class HomePage {
     });
 
     // Buat ikon baru
-    const customIcon = L.icon({
-      iconUrl: 'URL_TO_YOUR_ICON_IMAGE', // Ganti dengan URL gambar ikon
+    const customIcon = L.divIcon({
+      className: 'fa-marker', // Tambahkan kelas untuk styling
+      html: '<i class="fa-solid fa-location-pin" style="font-size: 24px; color: red;"></i>', // Ikon Font Awesome
       iconSize: [25, 41], // Ukuran ikon [lebar, tinggi]
       iconAnchor: [12, 41], // Titik yang akan menjadi posisi sebenarnya dari ikon
       popupAnchor: [1, -34], // Titik yang akan menjadi posisi popup
-    });
+  });
+
 
     // Menambahkan marker dengan popup
-    const marker = L.marker([-7.7742, 110.37]).addTo(this.map);
+    const marker = L.marker([-7.7742, 110.37], { icon: customIcon }).addTo(this.map);
     marker.bindPopup('<b>Lokasi Marker</b><br>Ini adalah Wajur.').openPopup();
-    
+
     // Layer control untuk base map
     this.baseMaps = {
       "OpenStreetMap": osm,
