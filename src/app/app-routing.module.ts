275570732d6696312fd6d59ea4372b0efad7e21c
mcnoelvito <noelvitohu@gmail.com>
2024-12-13 22:42:54 +0700

    move about and tabs to page folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home', // Sesuaikan dengan halaman utama Anda
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'list',
-    loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)
+    loadChildren: () => import('./tabs/list/list.module').then( m => m.ListPageModule)
   },
   {
     path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+    loadChildren: () => import('./tabs/about/about.module').then( m => m.AboutPageModule)
   },
 ];
 
