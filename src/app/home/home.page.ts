6d0d2c8d4949cc29beb0f902a914b3bd7ca5ba22
mcnoelvito <noelvitohu@gmail.com>
2024-12-16 00:19:40 +0700

    add place

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -53,7 +53,7 @@ export class HomePage {
     const locations = [
       { lat: -7.805142964499041 , lng: 110.36418960853538, title: 'Kompleks Kraton Yogyakarta',description: 'Merupakan sebuah kompleks bangunan keraton tempat tinggal keluarga Kesultanan Ngayogyakarta Hadiningrat.' },
       { lat: -7.800918, lng: 110.364179, title: 'Kompleks Gedung Agung',description: 'Merupakan salah satu istana kepresidenan Republik Indonesia yang terletak di Kota Yogyakarta.' },
-      { lat: -7.7735, lng: 110.36, title: 'Puro Pakualaman',description: 'Merupakan sebuah kompleks bangunan keraton tempat tinggal keluarga Kadipaten Pakualaman.' }
+      { lat: -7.8009511614206115, lng: 110.37614826857066, title: 'Puro Pakualaman',description: 'Merupakan sebuah kompleks bangunan keraton tempat tinggal keluarga Kadipaten Pakualaman.' }
     ];
 
     // Menambahkan marker untuk setiap lokasi
